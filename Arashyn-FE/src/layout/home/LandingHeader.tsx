d8c522ca1c55577e01ecbfe24530665407ac3b0e
Huy Phong <touman876@gmail.com>
2026-06-26 22:28:19 +0700

    feat(home-paage): complete home page

---
+++ Arashyn-FE/src/layout/home/LandingHeader.tsx
@@ -0,0 +1,40 @@
+import { BookOpen } from "lucide-react";
+
+export default function LandingHeader() {
+    return (
+        <header className="fixed inset-x-0 top-0 z-50 border-b border-white/10
+        bg-[#070b1ae0] backdrop-blur-2xl bg-background/50">
+            <div className="container mx-auto flex items-center justify-between px-6 py-4">
+                {/* Logo */}
+                <button className="flex items-center gap-3">
+                    <div className="border-white/10 bg-indigo-500/10 flex h-8 w-8 items-center justify-center rounded-lg border">
+                        <BookOpen className="size-4 text-indigo-400" />
+                    </div>
+                    <span className="text-lg font-bold tracking-tight">Arashyn</span>
+                </button>
+
+                {/* Navigation */}
+                <nav className="hidden items-center gap-8 md:flex">
+                    <button className="text-sm text-indigo-400">Features</button>
+                    <button className="text-white/60 hover:text-white text-sm">
+                        About
+                    </button>
+                    <button className="text-white/60 hover:text-white text-sm">
+                        Support
+                    </button>
+                    <button className="text-white/60 hover:text-white text-sm">
+                        Pricing
+                    </button>
+                </nav>
+
+                {/* Actions */}
+                <div className="flex items-center gap-3">
+                    <button className="text-white/60 px-4 py-2 text-sm">Log in</button>
+                    <button className="bg-indigo-500 shadow-[0_0_24px_rgba(99,102,241,0.4)] rounded-xl px-5 py-2 text-sm font-semibold text-white">
+                        Get Started
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
+}

ec25b8cb7ff8cd0c909ccd9ed09ac3d47b4ba095
Huy Phong <touman876@gmail.com>
2026-06-29 15:26:17 +0700

    fix(BE): handle missing exception

--- Arashyn-BE/src/main/java/com/arashi/edu/arashynbe/config/SecurityConfig.java
+++ Arashyn-BE/src/main/java/com/arashi/edu/arashynbe/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(
                             "/auth/**",
+                            "/error",
                             "/v3/api-docs/**",
                             "/swagger-ui/**",
                             "/swagger-ui.html"
